1ecf4813bded3644b6d88f9709cbf3e959f76cb0
Nikita <nsavchuk224@gmail.com>
2021-10-30 15:47:37 +0300

    Add models

---
+++ OnlineShop.Data/Models/Product.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using OnlineShop.Data.Enums;
+
+namespace OnlineShop.Data.Models
+{
+    public class Product
+    {
+        [Required]
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        
+        public SizeProduct SizeProduct { get; set; }
+        
+        public ColorProduct ColorProduct { get; set; }
+        
+        public string Description { get; set; }
+
+        public List<ProductImage> Images { get; set; }
+    }
+}
---
+++ OnlineShop.Data/Models/ProductImage.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace OnlineShop.Data.Models
+{
+    public class ProductImage
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Path { get; set; }
+    }
+}
