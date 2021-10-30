f0c77aa080414098e949369101eb6ed945060561
Nikita <nsavchuk224@gmail.com>
2021-10-30 15:48:45 +0300

    Add ViewModels

---
+++ OnlineShop.Web/ViewModels/Products/CreateProductViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Http;
+using OnlineShop.Data.Enums;
+
+namespace OnlineShop.Web.ViewModels.Products
+{
+    public class CreateProductViewModel
+    {
+        [Required]
+        public string Name { get; set; }
+        
+        [Required]
+        public decimal Price { get; set; }
+        
+        [Required]
+        public SizeProduct Size { get; set; }
+        
+        [Required]
+        public ColorProduct Color { get; set; }
+        
+        [Required]
+        public string Description { get; set; }
+        
+        [Required]
+        public IFormFileCollection Files { get; set; }
+    }
+}
---
+++ OnlineShop.Web/ViewModels/Products/ItemsProductViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using OnlineShop.Data.Models;
+
+namespace OnlineShop.Web.ViewModels.Products
+{
+    public class ItemsProductViewModel
+    {
+        public IEnumerable<Product> Products { get; set; }
+    }
+}
