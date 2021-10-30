c1c0dbb2240f741c554fddc9460ba13c219dda5a
Nikita <nsavchuk224@gmail.com>
2021-10-30 15:48:05 +0300

    Update Config.cs

--- OnlineShop.Web/Extension/Config.cs
+++ OnlineShop.Web/Extension/Config.cs
@@ -3,9 +3,8 @@
     public class Config
     {
         public static string ConnectionString { get; set; }
+        
+        public static string PathToPhoto { get; set; }
         public static string CompanyName { get; set; }
-        public static string CompanyPhone { get; set; }
-        public static string CompanyPhoneShort { get; set; }
-        public static string CompanyEmail { get; set; }
     }
 }
