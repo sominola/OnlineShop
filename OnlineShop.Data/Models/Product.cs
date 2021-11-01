92e3e2cba6d718d358c599bccff7f761f699670e
sominola <nsavchuk224@gmail.com>
2021-11-01 17:38:33 +0200

    Update Product.cs

--- OnlineShop.Data/Models/Product.cs
+++ OnlineShop.Data/Models/Product.cs
@@ -10,13 +10,15 @@ namespace OnlineShop.Data.Models
         [Required]
         public Guid Id { get; set; }
         public string Name { get; set; }
-        public decimal Price { get; set; }
+        public int Price { get; set; }
         
         public SizeProduct SizeProduct { get; set; }
         
         public ColorProduct ColorProduct { get; set; }
         
         public string Description { get; set; }
+        
+        public DateTime DateCreated { get; set; }
 
         public List<ProductImage> Images { get; set; }
     }
