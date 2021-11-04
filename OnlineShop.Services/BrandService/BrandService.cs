540a0398ed383e7d181b9824f4d7e41a8942c5b9
sominola <nsavchuk224@gmail.com>
2021-11-04 21:52:24 +0200

    Add Brand Service

---
+++ OnlineShop.Services/BrandService/BrandService.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using OnlineShop.Data;
+using OnlineShop.Data.Models;
+
+namespace OnlineShop.Services.BrandService
+{
+    public class BrandService : IBrandService
+    {
+        private readonly AppDbContext _db;
+
+        public BrandService(AppDbContext db)
+        {
+            _db = db;
+        }
+
+        public IQueryable<Brand> GetAllBrands()
+        {
+            return _db.Brands;
+        }
+
+        public IQueryable<Brand> FindBrandById(Guid id)
+        {
+            return _db.Brands.Where(b => b.Id == id);
+        }
+
+        public async Task<Brand> FindBrandsByNameAsync(string name)
+        {
+            return await _db.Brands.FirstOrDefaultAsync(b => b.Name == name);
+        }
+
+        public async Task CreateBrand(Brand brand)
+        {
+            _db.Brands.Add(brand);
+            await _db.SaveChangesAsync();
+        }
+    }
+}
